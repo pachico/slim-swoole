2cb44bb1a90c2f6d9f1bc022713ae0ebb62a5e52
Mariano Benítez Mulet <pachicodev@gmail.com>
2018-02-02 09:11:27 +0000

    Fine tunings

---
+++ examples/01-simple.php
@@ -0,0 +1,64 @@
+<?php
+
+use Pachico\SlimSwoole\BridgeManager;
+use Slim\Http;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+/**
+ * This is how you would normally bootstrap your Slim application
+ * For the sake of demonstration, we also add a simple middleware
+ * to check that the entire app stack is being setup and executed
+ * properly.
+ */
+$app = new \Slim\App();
+$app->any('/foo[/{myArg}]', function (Http\Request $request, Http\Response $response, array $args) {
+    $data = [
+        'args' => $args,
+        'body' => (string) $request->getBody(),
+        'parsedBody' => (string) $request->getParsedBody(),
+        'params' => $request->getParams(),
+        'headers' => $request->getHeaders(),
+        'uploadedFiles' => $request->getUploadedFiles()
+    ];
+
+    return $response->withJson($data);
+})->add(function (Http\Request $request, Http\Response $response, callable $next) {
+
+    $response->getBody()->write('BEFORE' . PHP_EOL);
+    $response = $next($request, $response);
+    $response->getBody()->write(PHP_EOL . 'AFTER');
+
+    return $response;
+});
+
+/**
+ * We instanciate the BridgeManager
+ */
+$bridgeManager = new BridgeManager($app);
+
+/**
+ * We start the Swoole server
+ */
+$http = new swoole_http_server("0.0.0.0", 8081);
+
+/**
+ * We register the on "start" event
+ */
+$http->on("start", function (\swoole_http_server $server) {
+    echo sprintf('Swoole http server is started at http://%s:%s', $server->host, $server->port), PHP_EOL;
+});
+
+/**
+ * We register the on "request event, which will use the BridgeManager to transform request, process it
+ * as a Slim request and merge back the response
+ *
+ */
+$http->on(
+    "request",
+    function (swoole_http_request $swooleRequest, swoole_http_response $swooleResponse) use ($bridgeManager) {
+        $bridgeManager->process($swooleRequest, $swooleResponse)->end();
+    }
+);
+
+$http->start();
--- examples/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require __DIR__ . '/slim.php';
-
-$app->run();
